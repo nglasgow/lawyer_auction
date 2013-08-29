15aa2c98c98894452280f82035e92fff2f2ddcc7
Nicole Glasgow <nicole.glasgow@ymail.com>
2013-08-29 13:24:05 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LawyerAuction::Application.config.secret_token = '21f3c0a1967c6dcdde7f3350879ef6e28dc8d8a7e93aa358378777aba659cf4494ab5a91536fd20c2cfa0071b946a04b642f15023810229034d182a1d94cca91'
